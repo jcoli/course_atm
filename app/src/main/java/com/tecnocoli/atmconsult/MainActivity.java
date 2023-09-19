2fa2c17bc43b90c4493d3724ebd2ffa977e7e209
jcoli <jefersoncoli@gmail.com>
2023-09-19 07:50:23 -0300

    190823-0749

--- app/src/main/java/com/tecnocoli/atmconsult/MainActivity.java
+++ app/src/main/java/com/tecnocoli/atmconsult/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tecnocoli.atmconsult;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -29,19 +31,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-//        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmail();
+            }
+        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_contact, R.id.nav_tools)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_contact, R.id.nav_tools, R.id.nav_about)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
@@ -61,5 +62,20 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void sendEmail(){
+//        String address = "https://www.google.com/maps/place/R.+Nilo+Pe%C3%A7anha+-+Santa+Paula,+S%C3%A3o+Caetano+do+Sul+-+SP,+09521-310/@-23.6171602,-46.5712986,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce5ce967e994d7:0x6fdd14575e9356a6!8m2!3d-23.6171651!4d-46.5687237!16s%2Fg%2F1ymvgh829?authuser=0&entry=ttu";
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(address));
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"jefersoncoli@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Contato pelo app");
+        intent.putExtra(Intent.EXTRA_TEXT, "Mensagem automatica");
+        intent.setType("text/plain");
+        String title = "Send";
+
+
+        startActivity(Intent.createChooser(intent, title));
     }
 }
